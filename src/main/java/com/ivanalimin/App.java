6b1a9ad32e0854c82de2a7c7bba4f1769f3e9c07
Ivan Alimin <aliminivan1990@gmail.com>
2024-08-14 19:43:33 +0300

    Practical task - Collection - filtering
    - add task description

--- src/main/java/com/ivanalimin/App.java
+++ src/main/java/com/ivanalimin/App.java
@@ -1,11 +1,15 @@
 package com.ivanalimin;
 
 import java.util.Arrays;
+/*
+Напишите метод filter, который принимает на вход массив любого типа, вторым арументом метод должен принимать клас,
+реализующий интерфейс Filter, в котором один метод - Object apply(Object o).
 
+Метод должен быть реализован так чтобы возращать новый масив, к каждому элементу которого была применена функция apply
+ */
 public class App {
 
     public static void main(String[] args) {
-
     }
 
     public static <T> T[] filter(T[] array, Filter filter) {
